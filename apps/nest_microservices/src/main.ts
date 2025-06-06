e99a00d9bbc363d67c350a71d505cef0316b345b
Ali-Adel-Nour <aliiiadel841@gmail.com>
2025-06-06 21:34:56 +0300

    adding husky confgirations

--- apps/nest_microservices/src/main.ts
+++ apps/nest_microservices/src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.useGlobalPipes(new ValidationPipe({whitelist:true}))
-  app.setGlobalPrefix(globalPrefix);
+   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3001;
   await app.listen(port);
   Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
